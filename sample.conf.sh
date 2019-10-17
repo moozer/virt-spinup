b119a5df73e32ee2e2eec7c436f9f884d118a5ca
Big Moozer <bigmoozer@gmail.com>
2019-10-17 13:36:20 +0200

    added sample file

---
+++ sample.conf.sh
@@ -0,0 +1,8 @@
+VMHOST="kvm-host"
+SOURCEHOST="deb10-base"
+PUBKEYFILE="~/.ssh/id_rsa.pub"
+SPINUPSCRIPT="/opt/virt-spinup/spinupmachine.sh"
+PASSWDFILE="passwords.log"
+USERNAME="sysuser"
+ANSIBLE_PASSWDFILE="passwords.yml"
+
